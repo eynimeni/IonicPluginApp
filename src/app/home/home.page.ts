506d223109d110ee7dff9757f50516e770258e11
eynimeni <maganatte@yahoo.de>
2022-12-14 12:28:45 +0100

    cleaned up home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,10 +2,6 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {Flashlight} from "@awesome-cordova-plugins/flashlight/ngx";
 import {Geolocation} from "@awesome-cordova-plugins/geolocation/ngx";
 
-import {Observable, timeout} from "rxjs";
-//import {Map, tileLayer} from 'leaflet';
-//import * as Leaflet from 'leaflet';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -15,28 +11,10 @@ export class HomePage implements OnInit {
 
   public latitude = 0
   public longitude = 0
-  //public watching: Observable<Geoposition | PositionError>
-  public latit2 = 0
-  public longi2 = 0
   public location: any
 
-  /*
-  @ViewChild('map') mapRef: ElementRef<HTMLElement> | undefined;
-  map: Map;
-  center: any = {
-    lat: 48.3,
-    long: 17
-  };
-  location: any = {
-    lat: '',
-    long: '',
-  }
 
-   */
-
-  constructor(public flashlight: Flashlight, private geolocation: Geolocation,
-              // public map : Map,
-  ) {
+  constructor(public flashlight: Flashlight, private geolocation: Geolocation) {
 
     this.geolocation.getCurrentPosition().then((resp) => {
       this.latitude = resp.coords.latitude
@@ -47,14 +25,10 @@ export class HomePage implements OnInit {
 
     this.location = this.geolocation.watchPosition()
     this.location.subscribe((resp: any) => {
-      this.longi2 = resp.coords.longitude
+      this.latitude = resp.coords.longitude
+      this.longitude = resp.coords.latitude
     })
 
-
-
-
-    //leaflet integrated in index.html und homepage.html aber wie kann ich es ansprechen? wo muss ich wie importieren?
-    //und alles was die bewegung watcht hab ich mal wieder auskommentiert, damit es lauffähig ist
 
   }
 
@@ -68,20 +42,7 @@ export class HomePage implements OnInit {
   }
 
 
-    watchPosition() {
 
-  }
-
-
-
-  /*
-    private createLeafletMap() {
-      this.map = new Map("map").setView(this.center, 13);
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© Angular LeafLet',
-      }).addTo(this.map);
-    };
-   */
 
 
   switchOnTorch() {
